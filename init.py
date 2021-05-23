3d03c68ee6b8f0c1ece6c8b9c9c4fc73e013307f
root <root@ip-172-31-34-28.ap-northeast-2.compute.internal>
2021-05-23 13:31:07 +0000

    Re-apply .gitignore

--- init.py
+++ init.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3.7
+
+from flask import Flask, render_template, request
+import sys
+sys.path.append("/ROKArmy-wage-check/ROKArmy-wage-check/python")
+from wage_calculator import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+	query = request.args.to_dict()
+	if bool(query) == True:
+		ipdae = query['ipdae']
+		main()
+		return None
 	return render_template('index.html')
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port='5000',debug=True)
